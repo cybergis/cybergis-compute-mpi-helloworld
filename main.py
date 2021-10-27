ba6d4b41bcefe2cbe8897a7d66621b518072866a
Zhiyu(Drew) Li <zyli2004@gmail.com>
2021-10-27 00:15:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ hostname = MPI.Get_processor_name()
 print("Hello World ! main.py {}/{}@{}".format(rank, size, hostname))
 
 if rank == 0:
-   pp.pprint(dict(os.environ), width=1)
+   my_environ_keys = ["data_folder", "executable_folder", "hpc", "job_id", "result_folder", "user_id", "SLURM_JOB_ID"]
+   environ_subset = {key: os.environ.get(key, "NULL") for key in my_environ_keys}
+   pp.pprint(environ_subset, width=1)
 comm.Barrier()
 
 

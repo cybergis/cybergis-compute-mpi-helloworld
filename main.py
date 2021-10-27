3058e0ca53963cdd5d0d91810de5a910504a7c63
Zhiyu(Drew) Li <zyli2004@gmail.com>
2021-10-26 23:49:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("main")
+from mpi4py import MPI
+import os
+import pprint
+
+# init mpi
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+hostname = MPI.Get_processor_name()
+
+
+print("Hello World ! main.py {}/{}@{}".format(rank, size, hostname))
+
+if rank == 0:
+   pprint(os.environ)
+comm.Barrier()
+
+
+print("Done! main.py {}/{}@{}".format(rank, size, hostname))

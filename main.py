9ca3cf10776e658f0262e3cac49a83bed670d947
Zhiyu(Drew) Li <zyli2004@gmail.com>
2021-10-27 00:20:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from mpi4py import MPI
 import os
 import pprint as pp
+import shutil
+from mpi4py import MPI
 
 # init mpi
 comm = MPI.COMM_WORLD
@@ -8,14 +9,20 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 hostname = MPI.Get_processor_name()
 
-
+# all processes to print Hello World
 print("Hello World ! main.py {}/{}@{}".format(rank, size, hostname))
 
+# only rank 0 does something
 if rank == 0:
+   # print some environment variables
    my_environ_keys = ["data_folder", "executable_folder", "hpc", "job_id", "result_folder", "user_id", "SLURM_JOB_ID"]
    environ_subset = {key: os.environ.get(key, "NULL") for key in my_environ_keys}
    pp.pprint(environ_subset, width=1)
+   
+   #copy input file(s) to result_folder
+   shutil.copytree(environ_subset["data_folder"], environ_subset["result_folder"])
+   
 comm.Barrier()
 
-
+# all processes to print Done
 print("Done! main.py {}/{}@{}".format(rank, size, hostname))
